aeee21239dce5a9b0e6b2fb192a4f9819b08565f
Christophe Riolo <christophe.riolo@qvik.fi>
2022-03-09 23:09:00 +0200

    Update CI to use multiple ocaml versions

--- test/support/test_runner.ml
+++ test/support/test_runner.ml
@@ -1,6 +1,6 @@
 (** Main entry point for our test runner.
 
-    This aggregates all the test suites and call Alcotes to run them. When
+    This aggregates all the test suites and call Alcotest to run them. When
     creating a new test suite, don't forget to add it here! *)
 
 let () =
