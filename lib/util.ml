f5b67fd717c4d33405b74c3280091feb14c77d85
Christophe Riolo <christophe.riolo@qvik.fi>
2022-12-12 22:14:35 +0200

    Delete time entries
    Closes #17

--- lib/util.ml
+++ lib/util.ml
@@ -1,8 +1,25 @@
 open Piaf
 
-let status_200_or_error (response : Response.t) : (string, Error.t) Lwt_result.t
+let format_response message (response : Response.t)
+    : (string, Error.t) Lwt_result.t
+  =
+  let ( let* ) = Lwt_result.bind in
+  let* body = Body.to_string response.body in
+  Buffer.add_string Format.stdbuf message ;
+  Buffer.add_string Format.stdbuf "\n" ;
+  Piaf.Response.pp_hum Format.str_formatter response ;
+  Buffer.add_string Format.stdbuf "\n\n" ;
+  Buffer.add_string Format.stdbuf body ;
+  let s = Buffer.contents Format.stdbuf in
+  Buffer.clear Format.stdbuf ; Lwt_result.return s
+
+let status_200_or_error message (response : Response.t)
+    : (string, Error.t) Lwt_result.t
   =
   let open Lwt_result in
   if Status.is_successful @@ response.status
   then Body.to_string response.body
-  else bind (Body.to_string response.body >|= fun s -> (`Msg s :> Error.t)) fail
+  else
+    bind
+      (format_response message response >|= fun s -> (`Msg s :> Error.t))
+      fail
